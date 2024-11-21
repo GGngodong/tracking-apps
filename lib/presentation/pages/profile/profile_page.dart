8bd035b378d5e1d8220fe28b92e8b766bb454e1a
GGngodong <ikkoy123@gmail.com>
2024-11-21 10:43:14 +0700

    sudah ada tapi kosong

--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,6 +12,27 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _appBar(),
+    );
+  }
+  AppBar _appBar(){
+    return AppBar(
+      title: Text(
+        'Profile',
+        style: TextStyle(
+          fontSize: 20.sp,
+          fontWeight: FontWeight.w700
+        ),
+      ),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          color: AppColors.primary
+        ),
+      ),
+      centerTitle: true,
+      elevation: 0,
+    );
   }
 }
