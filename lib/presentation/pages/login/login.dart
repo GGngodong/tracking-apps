5fa13770dc524c13d60defa5755a0cc147f28c4c
GGngodong <ikkoy123@gmail.com>
2024-11-22 15:48:25 +0700

    benerin login.dart rapihin codenya

--- lib/presentation/pages/login/login.dart
+++ lib/presentation/pages/login/login.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/presentation/component/custom_button.dart';
+import 'package:tracking_apps/presentation/component/custom_text_field.dart';
+import 'package:tracking_apps/presentation/component/divider_text.dart';
 import 'package:tracking_apps/presentation/component/header.dart';
+import 'package:tracking_apps/presentation/component/header_title.dart';
+import 'package:tracking_apps/presentation/component/socialmedia_button.dart';
+import 'package:tracking_apps/presentation/component/title_auth.dart';
+import 'package:tracking_apps/presentation/main_page.dart';
+import 'package:tracking_apps/presentation/pages/register/register.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -31,35 +40,104 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Header(
-                imageBg: 'assets/home/indonesia-nobg.png',
-                imageFg: 'assets/home/dahana.png',
-                height: 100.h,
-                topFg: 80,
-                leftFg: 0,
-                rightFg: 0,
-                topBg: 80,
-                leftBg: 0,
-                rightBg: 0,
-              ),
-              Expanded(
-                child: Padding(
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Header(
+                  heightSizedBox: 250.h,
+                  imageBg: 'assets/home/indonesia-nobg.png',
+                  imageFg: 'assets/home/dahana.png',
+                  height: 100.h,
+                  topFg: 80,
+                  leftFg: 0,
+                  rightFg: 0,
+                  topBg: 80,
+                  leftBg: 0,
+                  rightBg: 0,
+                ),
+                Padding(
                   padding: EdgeInsets.symmetric(horizontal: 16.w),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-
+                      HeaderTitle(title: 'Masuk', subTitle: 'Selamat Datang!'),
+                      SizedBox(
+                        height: 22.h,
+                      ),
+                      CustomTextField(
+                          hintText: 'Masukan email anda', header: 'Email'),
+                      SizedBox(
+                        height: 16.h,
+                      ),
+                      CustomTextField(
+                        hintText: 'Masukan password anda',
+                        header: 'Password',
+                        isPassword: true,
+                      ),
+                      SizedBox(
+                        height: 22.h,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            'Lupa Password?',
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.tertiary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24.h,
+                      ),
+                      CustomButton(
+                        text: 'Sign In',
+                        onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MainPage(statusCode: 200),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 32.h,
+                      ),
+                      DividerText(text: 'Masuk dengan'),
+                      SizedBox(
+                        height: 32.h,
+                      ),
+                      SocialMediaButton(),
+                      SizedBox(
+                        height: 32.h,
+                      ),
+                      TitleAuth(
+                        fun: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisterPage(),
+                          ),
+                        ),
+                        firstText: 'Belum mempunyai akun?',
+                        secondText: ' Daftar',
+                      ),
+                      SizedBox(
+                        height: 32.h,
+                      ),
                     ],
                   ),
-                ),
-              )
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
