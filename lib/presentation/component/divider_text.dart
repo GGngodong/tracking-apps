34df5657615dda6eb84469a01fb04ce4b3c95abb
GGngodong <ikkoy123@gmail.com>
2024-11-22 15:51:41 +0700

    add divider text so it can be used on other pages

---
+++ lib/presentation/component/divider_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+
+class DividerText extends StatelessWidget {
+  final String text;
+
+  const DividerText({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Expanded(child: Divider()),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.w),
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w400,
+                color: AppColors.lightGrey),
+          ),
+        ),
+        const Expanded(child: Divider())
+      ],
+    );
+  }
+}
