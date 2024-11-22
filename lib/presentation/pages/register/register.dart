2ab47de550e643854c696080279563e51c53239a
GGngodong <ikkoy123@gmail.com>
2024-11-22 15:47:26 +0700

    bikin page register.dart untuk register user

---
+++ lib/presentation/pages/register/register.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/main.dart';
+import 'package:tracking_apps/presentation/component/custom_button.dart';
+import 'package:tracking_apps/presentation/component/custom_text_field.dart';
+import 'package:tracking_apps/presentation/component/divider_text.dart';
+import 'package:tracking_apps/presentation/component/socialmedia_button.dart';
+import 'package:tracking_apps/presentation/component/title_auth.dart';
+import 'package:tracking_apps/presentation/main_page.dart';
+import 'package:tracking_apps/presentation/pages/login/login.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: ClampingScrollPhysics(),
+        child: Stack(
+          children: [
+            _headerRegister(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 240.h,
+                  ),
+                  CustomTextField(
+                      hintText: 'Masukan username anda', header: 'Username'),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  CustomTextField(
+                      hintText: 'Masukan email anda', header: 'Email'),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  CustomTextField(
+                    hintText: 'Masukan password anda',
+                    header: 'Password',
+                    isPassword: true,
+                  ),
+                  SizedBox(
+                    height: 46.h,
+                  ),
+                  CustomButton(text: 'Sign up', onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MainPage(statusCode: 200),
+                    ),
+                  ),),
+                  SizedBox(
+                    height: 32.h,
+                  ),
+                  DividerText(text: 'Masuk dengan'),
+                  SizedBox(
+                    height: 32.h,
+                  ),
+                  SocialMediaButton(),
+                  SizedBox(
+                    height: 32.h,
+                  ),
+                  TitleAuth(
+                    fun: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
+                      ),
+                    ),
+                    firstText: 'Sudah mempunyai akun?',
+                    secondText: ' Masuk',
+                  ),
+                  SizedBox(
+                    height: 32.h,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerRegister() {
+    return SizedBox(
+      height: 300.h,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: double.infinity,
+              height: 220.h,
+              decoration: BoxDecoration(gradient: suratIzinCategory),
+            ),
+          ),
+          Positioned(
+            top: 80,
+            left: 0,
+            right: 0,
+            child: Image.asset('assets/home/indonesia-nobg.png'),
+          ),
+          Positioned(
+            top: 120,
+            left: 20,
+            right: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Buat Akun',
+                  style: TextStyle(
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 8.h,
+                ),
+                Text(
+                  'Silahkan isi dibawah ini',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
