3c81bda6c00311fbe1d1c34399305fb42f01af3c
GGngodong <ikkoy123@gmail.com>
2024-11-22 15:46:36 +0700

    add tittle auth "sudah punya akun? daftar"

---
+++ lib/presentation/component/title_auth.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/main.dart';
+
+class TitleAuth extends StatelessWidget {
+  final String firstText;
+  final String secondText;
+  final VoidCallback fun;
+
+  const TitleAuth(
+      {super.key, required this.secondText, required this.firstText, required this.fun});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: GestureDetector(
+        onTap: fun,
+        child: Text.rich(
+          TextSpan(
+            text: firstText,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w500,
+              color: AppColors.lightGrey,
+            ),
+            children: [
+              TextSpan(
+                text: secondText,
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.tertiary,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
