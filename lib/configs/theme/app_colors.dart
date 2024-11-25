982270c2a4dbe515a0bf0985cb213afe1965f0db
GGngodong <ikkoy123@gmail.com>
2024-11-25 16:48:50 +0700

    refactor nama warna

--- lib/configs/theme/app_colors.dart
+++ lib/configs/theme/app_colors.dart
@@ -4,11 +4,11 @@ class AppColors {
   static const primary = Color(0xff489FB5);
   static const secondary = Color(0xff16697A);
   static const tertiary = Color(0xff82C0CC);
-  static const grey = Color(0xff000000);
   static const lightGrey = Color(0xff808080);
+  static const whitePage = Color(0xfff9fafe);
 }
 
-LinearGradient suratIzinCategory = const LinearGradient(
+LinearGradient headerAppBar = const LinearGradient(
   colors: [Color.fromRGBO(180, 222, 231, 1), Color.fromRGBO(129, 213, 229, 1)],
   begin: Alignment.centerLeft,
   end: Alignment.centerRight,
