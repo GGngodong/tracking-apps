eca2f2eeecc4f04a366ac60bf5b2142c46beaf93
GGngodong <ikkoy123@gmail.com>
2024-11-25 16:48:30 +0700

    tambahin card dan lain2 untuk notification_page.dart

--- lib/presentation/pages/notification/notification_page.dart
+++ lib/presentation/pages/notification/notification_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/presentation/component/card_notification.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -10,6 +13,70 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //backgroundColor: Colors.amber,
+      backgroundColor: AppColors.whitePage,
+      appBar: _appBar(),
+      body: _body(context),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        'Notifikasi',
+        style: TextStyle(
+          fontSize: 20.sp,
+          fontWeight: FontWeight.w700,
+          color: Colors.white,
+        ),
+      ),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(gradient: headerAppBar),
+      ),
+      centerTitle: true,
+      elevation: 0,
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 30.h,
+          ),
+          Text(
+            'Notifikasi Surat',
+            style: TextStyle(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w700,
+              color: AppColors.primary,
+            ),
+          ),
+          SizedBox(
+            height: 30.h,
+          ),
+          ListView.separated(
+            shrinkWrap: true,
+            primary: false,
+            physics: NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              return CardNotification(notifikasi: 'Surat berhasil di upload');
+            },
+            separatorBuilder: (context, index) {
+              return SizedBox(
+                height: 10.h,
+              );
+            },
+            itemCount: 5,
+          )
+        ],
+      ),
+    );
   }
 }
