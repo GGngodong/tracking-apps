87645e836c49fee5d02e92290c77b8076719af00
GGngodong <ikkoy123@gmail.com>
2024-11-25 16:47:22 +0700

    tambah pdf picker untuk upload page

---
+++ lib/presentation/component/pdf_upload.dart
@@ -0,0 +1,140 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+
+class PdfUpload extends StatefulWidget {
+  final String header;
+
+  const PdfUpload({super.key, required this.header});
+
+  @override
+  State<PdfUpload> createState() => _PdfUploadState();
+}
+
+class _PdfUploadState extends State<PdfUpload> {
+  String? _fileName;
+  bool _isFileSelected = false;
+
+  Future<void> _pickPdf() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ['pdf']);
+
+    if (result != null) {
+      setState(() {
+        _fileName = result.files.single.name;
+        _isFileSelected = true;
+      });
+    }
+  }
+
+  void _removeFile() {
+    setState(() {
+      _fileName = null;
+      _isFileSelected = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.header,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w500,
+              color: AppColors.primary,
+            ),
+          ),
+          SizedBox(
+            height: 8.h,
+          ),
+          GestureDetector(
+            onTap: _pickPdf,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.r),
+                border: Border.all(
+                    width: 1.w,
+                    color: _isFileSelected
+                        ? AppColors.primary
+                        : AppColors.lightGrey),
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.cloud_upload,
+                        size: 40.sp,
+                        color: Colors.grey,
+                      ),
+                      _isFileSelected
+                          ? Text(
+                              _fileName!,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: AppColors.lightGrey,
+                              ),
+                            )
+                          : Text.rich(
+                              textAlign: TextAlign.center,
+                              TextSpan(
+                                text: 'Unggah',
+                                style: TextStyle(
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: AppColors.tertiary,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: ' dokumen anda',
+                                    style: TextStyle(
+                                      fontSize: 14.sp,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.lightGrey,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '\nPDF (maksimal 5 MB)',
+                                    style: TextStyle(
+                                      fontSize: 14.sp,
+                                      fontWeight: FontWeight.w500,
+                                      color: AppColors.lightGrey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                      SizedBox(
+                        height: 16.h,
+                      ),
+                      _isFileSelected
+                          ? TextButton(
+                              onPressed: _removeFile,
+                              child: Text(
+                                'Hapus dokumen',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            )
+                          : const SizedBox()
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
