4422e6770cd953a285edeab7d081395c383f4bb1
GGngodong <ikkoy123@gmail.com>
2024-11-25 16:48:39 +0700

    bikin card profil

--- lib/presentation/component/card_profil.dart
+++ lib/presentation/component/card_profil.dart
@@ -1,33 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
 import 'package:tracking_apps/presentation/component/ink_effect.dart';
 
-Widget cardProfile({
-  required BuildContext context,
-  String name = 'Login',
-  String role = '',
-  String mail = '',
-  String avatar = '',
-  bool isLoggedIn = false,
-}) {
-  return InkEffect(
-    boxDecoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(10.r),
-    ),
-    onTap: () {},
-    child: Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: 20.h,
-        horizontal: 20.w,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+class CardProfile extends StatelessWidget {
+  final String role;
+  final String email;
+  final String userName;
 
-        ],
+  const CardProfile({
+    super.key,
+    required this.userName,
+    required this.email,
+    required this.role,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final String typeRole;
+    final TextStyle fontRoleStyle;
+    if (role == 'admin') {
+      fontRoleStyle = TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+        fontSize: 12.sp,
+      );
+      typeRole = 'Admin';
+    } else if (role == 'user') {
+      fontRoleStyle = TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+        fontSize: 12.sp,
+      );
+      typeRole = 'User';
+    } else {
+      fontRoleStyle = TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+        fontSize: 12.sp,
+      );
+      typeRole = 'No Role';
+    }
+    return InkEffect(
+      boxDecoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.r),
       ),
-    ),
-  );
+      onTap: () {},
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 20.w),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              'assets/icons/user-profile.png',
+              width: 64.w,
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+            ),
+            SizedBox(
+              width: 20.w,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: role == 'admin'
+                        ? const Color(0xFFAF4848)
+                        : const Color(0xff308c24),
+                    borderRadius: BorderRadius.circular(16.r),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color.fromRGBO(0, 0, 0, 0.02),
+                        offset: Offset(0, 4),
+                        blurRadius: 8,
+                      )
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 5.5, 12, 5.5),
+                    child: Row(
+                      children: [
+                        Text(typeRole, style: fontRoleStyle),
+                      ],
+                    ),
+                  ),
+                ),
+                Text(
+                  userName,
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w700,
+                    color: AppColors.primary,
+                  ),
+                ),
+                Text(
+                  email,
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.primary,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/presentation/component/card_profil.dart';
+import 'package:tracking_apps/presentation/component/custom_button.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,26 +15,49 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      //backgroundColor: Colors.amber,
+      backgroundColor: AppColors.whitePage,
       appBar: _appBar(),
+      body: _body(context),
     );
   }
-  AppBar _appBar(){
+
+  AppBar _appBar() {
     return AppBar(
       title: Text(
         'Profile',
         style: TextStyle(
-          fontSize: 20.sp,
-          fontWeight: FontWeight.w700
-        ),
+            fontSize: 20.sp, fontWeight: FontWeight.w700, color: Colors.white),
       ),
       flexibleSpace: Container(
-        decoration: BoxDecoration(
-          color: AppColors.primary
-        ),
+        decoration: BoxDecoration(gradient: headerAppBar),
       ),
       centerTitle: true,
       elevation: 0,
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 30.h,
+          ),
+          const CardProfile(
+            userName: 'Kim Jong-Un',
+            email: 'kimjong.un@dahana.id',
+            role: 'admin',
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          CustomButton(text: 'Log Out', onPressed: () {}, isLogOut: true)
+        ],
+      ),
     );
   }
 }
