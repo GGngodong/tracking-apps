47bc5a0c7df9ad03ab11ecf8e817728bae1a07fb
GGngodong <ikkoy123@gmail.com>
2024-11-26 16:09:52 +0700

    panggil carddetailsurat di dalem container detail_surat.dart

--- lib/presentation/pages/detail/detail_surat.dart
+++ lib/presentation/pages/detail/detail_surat.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/presentation/component/card_detail.dart';
 
 class DetailSuratPage extends StatefulWidget {
   const DetailSuratPage({super.key});
@@ -14,6 +15,7 @@ class _DetailSuratPageState extends State<DetailSuratPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
+      body: _body(context),
     );
   }
 }
@@ -37,5 +39,34 @@ AppBar _appBar() {
 }
 
 Widget _body(BuildContext context) {
-  return Container();
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      SizedBox(
+        height: 16.h,
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Text(
+          'Detail Surat',
+          style: TextStyle(
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w700,
+            color: AppColors.primary,
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 10.h,
+      ),
+      CardDetailSurat(
+          date: 'date',
+          categorySurat: 'categorySurat',
+          noSurat: 'noSurat',
+          namaPerusahaan: 'namaPerusahaan',
+          namaDokumen: 'namaDokumen',
+          noSuratIzinMabes: 'noSuratIzinMabes'),
+    ],
+  );
 }
