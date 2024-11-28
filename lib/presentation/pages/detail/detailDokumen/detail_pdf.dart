c880fcb1efcf5b844e8eff04cb62c48b1b9b1182
GGngodong <ikkoy123@gmail.com>
2024-11-28 16:34:48 +0700

    add pdf veiwer page

---
+++ lib/presentation/pages/detail/detailDokumen/detail_pdf.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+
+class DetailPDFPage extends StatefulWidget {
+  const DetailPDFPage({super.key});
+
+  @override
+  State<DetailPDFPage> createState() => _DetailPDFPageState();
+}
+
+class _DetailPDFPageState extends State<DetailPDFPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whitePage,
+      appBar: _appBar(),
+      body: Container(
+        child: SfPdfViewer.network(
+            'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf'),
+      ),
+    );
+  }
+}
+
+AppBar _appBar() {
+  return AppBar(
+    title: Text(
+      'Detail Surat',
+      style: TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w700,
+        color: Colors.white,
+      ),
+    ),
+    flexibleSpace: Container(
+      decoration: BoxDecoration(gradient: headerAppBar),
+    ),
+    centerTitle: true,
+    elevation: 0,
+  );
+}
