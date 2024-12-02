661ceae05330c5128b2a9043c9a7427197888f8c
GGngodong <ikkoy123@gmail.com>
2024-12-02 15:45:30 +0700

    add editing function

--- lib/presentation/pages/detail/detail_surat.dart
+++ lib/presentation/pages/detail/detail_surat.dart
@@ -12,11 +12,123 @@ class DetailSuratPage extends StatefulWidget {
 }
 
 class _DetailSuratPageState extends State<DetailSuratPage> {
+  bool isAdmin = true;
+  bool isEditing = false;
+  String date = '';
+  String categorySurat = '';
+  String noSurat = '';
+  String namaPerusahaan = '';
+  String namaDokumen = '';
+  String noSuratIzinMabes = '';
+
+  void toggleEdit() {
+    setState(() {
+      isEditing = !isEditing;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
-      body: _body(context),
+      appBar: AppBar(
+        title: Text(
+          isAdmin && isEditing ? 'Edit Detail Surat' : 'Detail Surat',
+          style: TextStyle(
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(gradient: headerAppBar),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 16.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Text(
+                'Detail Surat',
+                style: TextStyle(
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.w700,
+                  color: AppColors.primary,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            CardDetailSurat(
+              date: date,
+              categorySurat: categorySurat,
+              namaDokumen: namaDokumen,
+              namaPerusahaan: namaPerusahaan,
+              noSurat: noSurat,
+              noSuratIzinMabes: noSuratIzinMabes,
+              isEditing: isEditing,
+              onFieldChanged: (field, value) {
+                if (field == 'date') date = value;
+                if (field == 'categorySurat') categorySurat = value;
+                if (field == 'noSurat') noSurat = value;
+                if (field == 'namaPerusahaan') namaPerusahaan = value;
+                if (field == 'namaDokumen') namaDokumen = value;
+                if (field == 'noSuratIzinMabes') noSuratIzinMabes = value;
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => DetailPDFPage(),
+                      ),
+                    ),
+                    child: Text(
+                      'Lihat Dokumen',
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ),
+                  if (isAdmin)
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            isEditing ? Color(0xFF39B43B) : Color(0xFFAF4848),
+                      ),
+                      onPressed: toggleEdit,
+                      child: Text(
+                        isEditing ? 'Save' : 'Edit Field',
+                        style: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.h,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
@@ -36,63 +148,5 @@ AppBar _appBar() {
     ),
     centerTitle: true,
     elevation: 0,
-  );
-}
-
-Widget _body(BuildContext context) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      SizedBox(
-        height: 16.h,
-      ),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Text(
-          'Detail Surat',
-          style: TextStyle(
-            fontSize: 20.sp,
-            fontWeight: FontWeight.w700,
-            color: AppColors.primary,
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 10.h,
-      ),
-      CardDetailSurat(
-        date: '28 Februari 2023',
-        categorySurat: 'OPS',
-        namaDokumen: 'Angkut Subang Bati',
-        namaPerusahaan: 'PT Dahana',
-        noSurat: 'B/008/I/2023/Korp-Jkt',
-        noSuratIzinMabes: 'SI/1128/I/YAN.2.10./2023',
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w),
-            child: GestureDetector(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) => DetailPDFPage(),
-                ),
-              ),
-              child: Text(
-                'Lihat Dokumen',
-                style: TextStyle(
-                  fontSize: 12.sp,
-                  fontWeight: FontWeight.w700,
-                  color: AppColors.primary,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ],
   );
 }
