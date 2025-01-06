44d0f27967e4d6a9f52a5f9e7e38714f30842a62
GGngodong <ikkoy123@gmail.com>
2025-01-06 09:39:34 +0700

    add dio client and failure

--- lib/common/failure.dart
+++ lib/common/failure.dart
@@ -1,22 +1,30 @@
 import 'package:equatable/equatable.dart';
 
+
 abstract class Failure extends Equatable {
   final String message;
-
   const Failure(this.message);
 
   @override
-  List<Object> get props => [message];
+  List<Object?> get props => [message];
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure(super.message);
+  const ServerFailure(String message) : super(message);
 }
 
 class NotFoundFailure extends Failure {
-  const NotFoundFailure(super.message);
+  const NotFoundFailure(String message) : super(message);
 }
 
 class ConnectionFailure extends Failure {
-  const ConnectionFailure(super.message);
+  const ConnectionFailure(String message) : super(message);
+}
+
+class UnauthorizedFailure extends Failure {
+  const UnauthorizedFailure(String message) : super(message);
+}
+
+class ValidationFailure extends Failure {
+  const ValidationFailure(String message) : super(message);
 }
