1a6003912b0ff8e770764733b6ebe08a7a9f9c53
GGngodong <ikkoy123@gmail.com>
2025-01-06 09:42:05 +0700

    add base_url file and shared preferences

---
+++ lib/common/base_url.dart
@@ -0,0 +1,16 @@
+class BaseUrl {
+  // static const baseUrl = 'http://192.168.11.244:8000';
+  static const baseUrl = 'http://192.168.1.10:8000';
+  // USER
+  static const register = '$baseUrl/api/dev/users';
+  static const login = '$baseUrl/api/dev/users/login';
+  static const getUser = '$baseUrl/api/dev/current';
+  static const deleteUser = '$baseUrl/api/dev/users/logout';
+  //
+  static const uploadPermit = '$baseUrl/api/dev/permit-letters/upload';
+  static const getPermit = '$baseUrl/api/dev/permit-letters/';
+  static const getPermitById = '$baseUrl/api/dev/permit-letters/';
+  static const searchPermit = '$baseUrl/api/dev/permit-letters/search';
+  static const editPermit = '$baseUrl/api/dev/permit-letters/edit/';
+  static const deletePermit = '$baseUrl/api/dev/permit-letters/delete/';
+}
---
+++ lib/data/local/shared_pref_helper.dart
@@ -0,0 +1,32 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefHelper {
+  static const String _tokenKey = 'token';
+  static const String _roleKey = 'role';
+
+  Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_tokenKey, token);
+  }
+
+  Future<void> saveRole(String role) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_roleKey, role);
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_tokenKey);
+  }
+
+  Future<String?> getRole() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_roleKey);
+  }
+
+  Future<void> clear() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_tokenKey);
+    await prefs.remove(_roleKey);
+  }
+}
