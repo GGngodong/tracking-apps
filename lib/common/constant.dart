153879950eace21f15863d62df3eeff62dbee4b9
GGngodong <ikkoy123@gmail.com>
2025-01-13 18:26:41 +0700

    change the constant

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-SnackBar noInternet = SnackBar(
-  width: 160,
-  content: Text(
-    'Koneksi internet bermasalah',
-    textAlign: TextAlign.center,
-    style: TextStyle(fontSize: 10.sp),
-  ),
-  behavior: SnackBarBehavior.floating,
-);
+class Constant {
+  static final DateFormat dateFormat = DateFormat('dd.MM.yyyy');
+  static final DateTime nullDate = DateTime.parse('0001-01-01T00:00:00Z').toUtc();
+  static final usernameRegex = RegExp(r'^[a-zA-Z0-9._]{3,20}$');
+  static final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  static final passwordRegex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+}
