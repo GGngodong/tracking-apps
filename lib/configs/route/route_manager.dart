33799bb31a013e7c9f04e5193d0c4f819246650b
GGngodong <ikkoy123@gmail.com>
2025-01-13 18:30:44 +0700

    add go router

---
+++ lib/configs/route/route_manager.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tracking_apps/common/role_middleware.dart';
+import 'package:tracking_apps/common/shared_preferance_service.dart';
+import 'package:tracking_apps/configs/route/routes.dart';
+import 'package:tracking_apps/presentation/blocs/auth/login/login_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
+import 'package:tracking_apps/presentation/main_page.dart';
+import 'package:tracking_apps/presentation/pages/beranda/home_page.dart';
+import 'package:tracking_apps/presentation/pages/login/login.dart';
+import 'package:tracking_apps/presentation/pages/notification/notification_page.dart';
+import 'package:tracking_apps/presentation/pages/profile/profile_page.dart';
+import 'package:tracking_apps/presentation/pages/register/register.dart';
+import 'package:tracking_apps/presentation/pages/splash/splash_screen.dart';
+import 'package:tracking_apps/presentation/pages/upload/upload_page.dart';
+import 'package:tracking_apps/presentation/test_main_page.dart';
+import 'package:tracking_apps/presentation/test_page.dart';
+
+final class RouterManager {
+  RouterManager._();
+
+  static GoRouter router({required String? userRole}) {
+    return GoRouter(
+      routes: [
+        GoRoute(
+          path: Routes.initial.path,
+          builder: (context, state) => const SplashScreen(),
+        ),
+        GoRoute(
+            path: Routes.navigation.path,
+            builder: (context, state) {
+              return BlocBuilder<ProfileBloc, ProfileState>(
+                builder: (context, profileState) {
+                  return MainPage(role: profileState.user!.role);
+                },
+              );
+            }
+        ),
+        GoRoute(
+            path: Routes.homePage.path,
+            builder: (context, state) => const HomePage()
+        ),
+        GoRoute(
+          path: Routes.uploadData.path,
+          builder: (context, state) => UploadPage()
+
+        ),
+        GoRoute(
+            path: Routes.notification.path,
+            builder: (context, state) => const NotificationPage()
+        ),
+        GoRoute(
+          path: Routes.profile.path,
+          builder: (context, state) => const ProfilePage(),
+        ),
+        GoRoute(
+          path: Routes.login.path,
+          builder: (context, state) => const LoginPage(),
+        ),
+        GoRoute(
+          path: Routes.register.path,
+          builder: (context, state) => const RegisterPage(),
+        ),
+      ],
+    );
+  }
+}
