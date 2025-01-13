075144114a38b5456f74af0f15dee2fff35f4cd4
GGngodong <ikkoy123@gmail.com>
2025-01-13 18:29:07 +0700

    add profile bloc, event, state and adding mixin. wiring through profile page

---
+++ lib/presentation/blocs/profile/profile_bloc.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tracking_apps/configs/network/http_response_model.dart';
+import 'package:tracking_apps/configs/network/user_service.dart';
+import 'package:tracking_apps/domain/entity/user_model.dart';
+
+part 'profile_event.dart';
+
+part 'profile_state.dart';
+
+class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
+  final UserService userService;
+
+  ProfileBloc({required this.userService}) : super(const ProfileState()) {
+    on<UpdateUser>((event, emit) async {
+      emit(ProfileState(user: event.user, isLoading: true));
+      try {
+        HttpResponseModel<dynamic> updateResponse =
+            await userService.update(userModel: event.user);
+        if (updateResponse.statusCode == 200) {
+          final user = UserModel.fromMap(updateResponse.data);
+          emit(UpdateUserSuccess(
+              user: user, message: updateResponse.message, isLoading: false));
+        } else {
+          emit(UpdateUserFailed(
+              user: event.user,
+              message: updateResponse.message,
+              isLoading: false));
+        }
+      } catch (error) {
+        emit(UpdateUserFailed(
+            user: event.user, message: error.toString(), isLoading: false));
+      }
+    });
+
+    on<SetUser>((event, emit) async {
+      emit(ProfileState(user: event.user));
+    });
+
+    on<SetLoading>((event, emit) async {
+      emit(ProfileState(isLoading: event.isLoading));
+    });
+  }
+}
--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,8 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tracking_apps/common/app_helper.dart';
+import 'package:tracking_apps/configs/route/routes.dart';
 import 'package:tracking_apps/configs/theme/app_colors.dart';
+import 'package:tracking_apps/presentation/blocs/auth/login/login_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
 import 'package:tracking_apps/presentation/component/card_profil.dart';
 import 'package:tracking_apps/presentation/component/custom_button.dart';
+
+import '../../../generated/locale_keys.g.dart';
+
+part 'profile_mixin.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,7 +23,7 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage> with ProfileMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,26 +50,37 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget _body(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 16.w),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 30.h,
-          ),
-          const CardProfile(
-            userName: 'Kim Jong-Un',
-            email: 'kimjong.un@dahana.id',
-            role: 'admin',
-          ),
-          SizedBox(
-            height: 20.h,
-          ),
-          CustomButton(text: 'Log Out', onPressed: () {}, isLogOut: true)
-        ],
-      ),
+    final LoginBloc loginBloc = BlocProvider.of<LoginBloc>(context);
+    return BlocBuilder<LoginBloc, LoginState>(
+      builder: (context, loginState ) {
+        return BlocBuilder<ProfileBloc, ProfileState>(
+          builder: (context, profileState) {
+            return SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  CardProfile(
+                    userName: '${profileState.user?.userName}',
+                    email: '${profileState.user?.email}',
+                    role: '${profileState.user?.role}',
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  CustomButton(text: 'Log Out', onPressed: () => _showLogOutDialog(context, loginBloc), isLogOut: true)
+                ],
+              ),
+            );
+          }
+        );
+      }
+
     );
   }
 }
