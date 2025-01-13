168145d6dc0fd0bc5453b88edff006ec8ea97883
GGngodong <ikkoy123@gmail.com>
2025-01-13 18:28:23 +0700

    fixing login bloc, event, state and adding login mixin, remove old login request parameters

--- lib/presentation/pages/login/login.dart
+++ lib/presentation/pages/login/login.dart
@@ -1,10 +1,17 @@
-import 'dart:convert';
-
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tracking_apps/common/app_helper.dart';
+import 'package:tracking_apps/configs/network/http_response_model.dart';
+import 'package:tracking_apps/configs/route/routes.dart';
 import 'package:tracking_apps/configs/theme/app_colors.dart';
-import 'package:tracking_apps/lib/network/api_service.dart';
+import 'package:tracking_apps/generated/locale_keys.g.dart';
+import 'package:tracking_apps/presentation/blocs/auth/login/login_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/auth/register/register_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
 import 'package:tracking_apps/presentation/component/custom_button.dart';
 import 'package:tracking_apps/presentation/component/custom_text_field.dart';
 import 'package:tracking_apps/presentation/component/divider_text.dart';
@@ -15,6 +22,8 @@ import 'package:tracking_apps/presentation/component/title_auth.dart';
 import 'package:tracking_apps/presentation/main_page.dart';
 import 'package:tracking_apps/presentation/pages/register/register.dart';
 
+part 'login_mixin.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -22,209 +31,118 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _isLoading = false;
-  bool _secureText = true;
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+class _LoginPageState extends State<LoginPage> with LoginMixin {
 
-  final _formKey = GlobalKey<FormState>();
-
-  showHide() {
-    setState(() {
-      _secureText = !_secureText;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return _page(context);
-  }
-
-  Future<void> alertDialogError(BuildContext context) {
-    return showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Login failed'),
-          content: Text('Error: Incorrect Email or Password!'),
-          actions: [
-            TextButton(
-                onPressed: () => Navigator.pop(context, 'OK'),
-                child: const Text('OK'))
-          ],
-        );
+    final LoginBloc _loginBloc = BlocProvider.of<LoginBloc>(context);
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        _listener(state);
       },
-    );
-  }
-
-  Widget _page(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: SingleChildScrollView(
-        physics: const ClampingScrollPhysics(),
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Header(
-                  heightSizedBox: 250.h,
-                  imageBg: 'assets/home/indonesia-nobg.png',
-                  imageFg: 'assets/home/dahana.png',
-                  height: 100.h,
-                  topFg: 80,
-                  leftFg: 0,
-                  rightFg: 0,
-                  topBg: 80,
-                  leftBg: 0,
-                  rightBg: 0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.w),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const HeaderTitle(
-                            title: 'Masuk', subTitle: 'Selamat Datang!'),
-                        SizedBox(
-                          height: 22.h,
-                        ),
-                        CustomTextField(
-                          textController: _emailController,
-                          hintText: 'Masukan email anda',
-                          header: 'Email',
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Email cannot be empty';
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(
-                          height: 16.h,
-                        ),
-                        CustomTextField(
-                          textController: _passwordController,
-                          hintText: 'Masukan password anda',
-                          header: 'Password',
-                          isPassword: true,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Password cannot be empty';
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(
-                          height: 22.h,
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Text(
-                              'Lupa Password?',
-                              style: TextStyle(
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.w500,
-                                color: AppColors.tertiary,
+      child: BlocBuilder<LoginBloc, LoginState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Header(
+                        heightSizedBox: 250.h,
+                        imageBg: 'assets/home/indonesia-nobg.png',
+                        imageFg: 'assets/home/dahana.png',
+                        height: 100.h,
+                        topFg: 80,
+                        leftFg: 0,
+                        rightFg: 0,
+                        topBg: 80,
+                        leftBg: 0,
+                        rightBg: 0,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.w),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const HeaderTitle(
+                              title: 'Masuk',
+                              subTitle: 'Selamat Datang!',
+                            ),
+                            SizedBox(height: 22.h),
+                            CustomTextField(
+                              textController: _emailTextEditingController,
+                              hintText: 'Masukan email anda',
+                              header: 'Email',
+                              onFieldSubmitted: (value) {
+                                _submit(_loginBloc);
+                              },
+                            ),
+                            SizedBox(height: 16.h),
+                            CustomTextField(
+                              textController: _passwordTextEditingController,
+                              hintText: 'Masukan password anda',
+                              header: 'Password',
+                              isPassword: true,
+                              onFieldSubmitted: (value) {
+                                _submit(_loginBloc);
+                              },
+                            ),
+                            SizedBox(height: 22.h),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  'Lupa Password?',
+                                  style: TextStyle(
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w500,
+                                    color: AppColors.tertiary,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 24.h,
-                        ),
-                        CustomButton(
-                          text: 'Sign In',
-                          onPressed: () async {
-                            final email = _emailController.text;
-                            final password = _passwordController.text;
-                            if (email.isNotEmpty && password.isNotEmpty) {
-                              if (_formKey.currentState?.validate() ?? false) {
-                                _login();
-                              }
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                    content: Text('Please fill in all fields')),
-                              );
-                            }
-                          },
-                          isLogOut: false,
-                        ),
-                        SizedBox(
-                          height: 32.h,
-                        ),
-                        const DividerText(text: 'Masuk dengan'),
-                        SizedBox(
-                          height: 32.h,
-                        ),
-                        const SocialMediaButton(),
-                        SizedBox(
-                          height: 32.h,
-                        ),
-                        TitleAuth(
-                          fun: () =>
-                              Navigator.push(
+                            SizedBox(height: 24.h),
+                            CustomButton(
+                              isLoading: state.isLoading,
+                              text: 'Sign In',
+                              onPressed: () {
+                                _submit(_loginBloc);
+                              },
+                              isLogOut: false,
+                            ),
+                            SizedBox(height: 32.h),
+                            const DividerText(text: 'Masuk dengan'),
+                            SizedBox(height: 32.h),
+                            const SocialMediaButton(),
+                            SizedBox(height: 32.h),
+                            TitleAuth(
+                              fun: () => Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                   builder: (BuildContext context) =>
                                   const RegisterPage(),
                                 ),
                               ),
-                          firstText: 'Belum mempunyai akun?',
-                          secondText: ' Daftar',
+                              firstText: 'Belum mempunyai akun?',
+                              secondText: ' Daftar',
+                            ),
+                            SizedBox(height: 32.h),
+                          ],
                         ),
-                        SizedBox(
-                          height: 32.h,
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
-  }
-
-  _showMsg(msg) {
-    final snackBar = SnackBar(
-      content: Text(msg),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-
-  void _login() async {
-    setState(() {
-      _isLoading = true;
-    });
-    var data = {'email': _emailController, 'password': _passwordController};
-
-    var res = await ApiService().auth(data, '/users/login');
-    var body = json.decode(res.body);
-    if (body['success']) {
-      SharedPreferences localStorage = await SharedPreferences.getInstance();
-      localStorage.setString('token', json.encode(body['token']));
-      localStorage.setString('user', json.encode(body['user']));
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => MainPage(isAdmin: true)),
-      );
-    } else {
-      _showMsg(body['message']);
-    }
-
-    setState(() {
-      _isLoading = false;
-    });
   }
 }
---
+++ lib/presentation/pages/login/login_mixin.dart
@@ -0,0 +1,76 @@
+part of 'login.dart';
+
+mixin LoginMixin on State<LoginPage> {
+  late TextEditingController _emailTextEditingController;
+  late TextEditingController _passwordTextEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailTextEditingController = TextEditingController();
+    _passwordTextEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+  }
+
+  void _forgotPasswordListener(RegisterState state) async {
+    if (state is ForgotPasswordCheckSuccess) {
+      if (state.data != null && state.data!) {
+        if (state.verificationCode != null) {
+          context.go(Routes.verify.path);
+        }
+      }
+    } else if (state is ForgotPasswordCheckFailed) {
+      print('================== CURRENT STATE : $state ==================');
+      AppHelper.alertDialogMessage(context: context, content: LocaleKeys.non_existent_user_message.tr());
+    } else if (state is CheckFailed) {
+      AppHelper.alertDialogMessage(context: context, content: LocaleKeys.something_went_wrong.tr());
+      print('================== CURRENT STATE : $state ==================');
+    }
+  }
+
+  void _listener(LoginState state) {
+    final ProfileBloc profileBloc = BlocProvider.of<ProfileBloc>(context);
+    if (state is LoginSuccess) {
+      print('================== CURRENT STATE : $state ==================');
+      profileBloc.add(SetUser(user: state.user));
+      context.go(Routes.initial.path);
+      if (state.user.role == 'ADMIN') {
+        context.go(Routes.navigation.path, extra: true);
+      } else {
+        context.go(Routes.navigation.path, extra: false);
+      }
+    } else if (state is LoginFailed) {
+      print('================== CURRENT STATE  : $state ==================');
+
+      if (state.statusCode == 401) {
+        AppHelper.alertDialogMessage(context: context, content: LocaleKeys.check_your_information.tr());
+      } else {
+        AppHelper.alertDialogMessage(context: context, content: LocaleKeys.something_went_wrong.tr());
+      }
+    }
+  }
+
+  void _submit(LoginBloc loginBloc) {
+    HttpResponseModel httpResponseModel = AppHelper.checkEmailAndPassword(
+      email: _emailTextEditingController.text.trim(),
+      password: _passwordTextEditingController.text.trim(),
+    );
+    print('================== CURRENT : $loginBloc ==================');
+    if (httpResponseModel.statusCode == 200) {
+      loginBloc.add(
+        LoginButtonPressed(
+          email: _emailTextEditingController.text.trim(),
+          password: _passwordTextEditingController.text.trim(),
+        ),
+      );
+    } else {
+      AppHelper.alertDialogMessage(context: context, content: httpResponseModel.message);
+    }
+  }
+}
