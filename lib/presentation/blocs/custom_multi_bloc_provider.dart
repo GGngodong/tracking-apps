a76930051b9a39b600978000a46497f9ebd909f0
GGngodong <ikkoy123@gmail.com>
2025-01-13 18:27:16 +0700

    adding custom multi bloc provider

---
+++ lib/presentation/blocs/custom_multi_bloc_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tracking_apps/configs/network/user_service.dart';
+import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
+
+import 'auth/login/login_bloc.dart';
+import 'auth/register/register_bloc.dart';
+
+class CustomMultiBlocProvider extends StatelessWidget {
+  final Widget child;
+  const CustomMultiBlocProvider({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginBloc(userService: UserService())),
+        BlocProvider(create: (context) => RegisterBloc(userService: UserService())),
+        BlocProvider(create: (context) => ProfileBloc(userService: UserService())),
+      ],
+      child: child,
+    );
+  }
+}
