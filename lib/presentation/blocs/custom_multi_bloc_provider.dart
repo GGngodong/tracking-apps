14142ce3a39f7ff6057c3b6a3c26a438bd3c4aa7
GGngodong <ikkoy123@gmail.com>
2025-01-19 23:11:13 +0700

    add PermitLetterBloc to custom_multi_bloc_provider.dart

--- lib/presentation/blocs/custom_multi_bloc_provider.dart
+++ lib/presentation/blocs/custom_multi_bloc_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tracking_apps/configs/network/user_service.dart';
-import 'package:tracking_apps/presentation/blocs/beranda/upload/upload_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/permit/listPermit/get_permit_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/permit/upload/upload_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
 
 import 'auth/login/login_bloc.dart';
@@ -9,16 +10,23 @@ import 'auth/register/register_bloc.dart';
 
 class CustomMultiBlocProvider extends StatelessWidget {
   final Widget child;
+
   const CustomMultiBlocProvider({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => LoginBloc(userService: UserService())),
-        BlocProvider(create: (context) => RegisterBloc(userService: UserService())),
-        BlocProvider(create: (context) => ProfileBloc(userService: UserService())),
-        BlocProvider(create: (context) => UploadBloc(userService: UserService()),)
+        BlocProvider(
+            create: (context) => LoginBloc(userService: UserService())),
+        BlocProvider(
+            create: (context) => RegisterBloc(userService: UserService())),
+        BlocProvider(
+            create: (context) => ProfileBloc(userService: UserService())),
+        BlocProvider(
+          create: (context) => UploadBloc(userService: UserService())),
+        BlocProvider(
+            create: (context) => PermitLetterBloc(userService: UserService()))
       ],
       child: child,
     );
