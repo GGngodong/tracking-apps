aabf6a38564af74887169f11c7068e0e8844cb20
GGngodong <ikkoy123@gmail.com>
2025-02-15 20:10:54 +0700

    add approved rejected and pending bloc

--- lib/presentation/blocs/custom_multi_bloc_provider.dart
+++ lib/presentation/blocs/custom_multi_bloc_provider.dart
@@ -3,7 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tracking_apps/configs/network/user_service.dart';
 import 'package:tracking_apps/presentation/blocs/permit/detail/get_detail_permit_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/edit/edit_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/permit/listPermit/approved/get_approved_permit_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/listPermit/get_permit_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/permit/listPermit/pending/get_pending_permit_bloc.dart';
+import 'package:tracking_apps/presentation/blocs/permit/listPermit/reject/get_reject_permit_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/search/search_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/upload/upload_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
@@ -35,6 +38,15 @@ class CustomMultiBlocProvider extends StatelessWidget {
                 DetailPermitLetterBloc(userService: UserService())),
         BlocProvider(create: (context) => EditBloc(userService: UserService())),
         BlocProvider(create: (context) => SearchBloc(userService: UserService())),
+        BlocProvider(
+            create: (context) =>
+                PermitLetterApprovedBloc(userService: UserService())),
+        BlocProvider(
+            create: (context) =>
+                PermitLetterRejectBloc(userService: UserService())),
+        BlocProvider(
+            create: (context) =>
+                PermitLetterPendingBloc(userService: UserService())),
       ],
       child: child,
     );
