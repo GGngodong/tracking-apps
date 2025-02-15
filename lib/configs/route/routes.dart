e40f107f3c39c930d7655c7d4665e4b3eb657103
GGngodong <ikkoy123@gmail.com>
2025-02-15 20:09:59 +0700

    add new routes

--- lib/configs/route/routes.dart
+++ lib/configs/route/routes.dart
@@ -2,18 +2,20 @@ enum Routes {
   initial('/'),
   navigation('/navigation'),
   homePage('/home'),
+  statusPage('/status'),
   search('/search'),
   uploadData('/upload'),
   notification('/notification'),
   login('/login'),
   register('/register'),
   listPermit('/list_permit'),
+  rejection('/rejection'),
   verify('/verify'),
   profile('/profile'),
   settings('/settings'),
-  updatePassword('/update_password'),
-  ;
+  updatePassword('/update_password');
 
   final String path;
+
   const Routes(this.path);
 }
