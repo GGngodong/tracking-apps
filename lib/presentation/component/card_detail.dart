22d88c59db9d9ea79e3e3e082c4128dc18293664
GGngodong <ikkoy123@gmail.com>
2025-02-16 21:23:38 +0700

    remove unused type status and font style for type status

--- lib/presentation/component/card_detail.dart
+++ lib/presentation/component/card_detail.dart
@@ -13,6 +13,7 @@ class CardDetailSurat extends StatefulWidget {
   final String namaDokumen;
   final String noSuratIzinMabes;
   final String processStatus;
+  final String uploadStatus;
   final String id;
 
   const CardDetailSurat(
@@ -24,7 +25,7 @@ class CardDetailSurat extends StatefulWidget {
       required this.namaDokumen,
       required this.noSuratIzinMabes,
       required this.id,
-      required this.processStatus});
+      required this.processStatus, required this.uploadStatus});
 
   @override
   State<CardDetailSurat> createState() => _CardDetailSuratState();
@@ -34,89 +35,6 @@ class _CardDetailSuratState extends State<CardDetailSurat> {
 
   @override
   Widget build(BuildContext context) {
-    final String typeStatus;
-    final TextStyle fontStatusStyle;
-    if (widget.processStatus == 'Draft Created') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFFD3D3D3),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Draft Created';
-    } else if (widget.processStatus == 'Archiving') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFFA9A9A9),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Archiving';
-    } else if (widget.processStatus == 'Submission') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFF007BFF),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Submission';
-    } else if (widget.processStatus == 'Verification') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFFFFFF00),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Verification';
-    } else if (widget.processStatus == 'Initial Approval') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFF90EE90),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Initial Approval';
-    } else if (widget.processStatus == 'Second Approval') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFF008000),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Second Approval';
-    } else if (widget.processStatus == 'Drafter') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFFFFA500),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Drafter';
-    } else if (widget.processStatus == 'Final Approval') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFF00FF00),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Final Approval';
-    } else if (widget.processStatus == 'Printing') {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFF800080),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'On Printing';
-    } else {
-      fontStatusStyle = TextStyle(
-        color: Color(0xFFAF4848),
-        fontWeight: FontWeight.w600,
-        fontSize: 12.sp,
-        fontFamily: 'Satoshi',
-      );
-      typeStatus = 'No Status';
-    }
     return Container(
       width: double.infinity,
       padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
@@ -125,6 +43,26 @@ class _CardDetailSuratState extends State<CardDetailSurat> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text(
+            'Status Permohonan',
+            style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Satoshi',
+              color: AppColors.primary,
+            ),
+          ),
+          SizedBox(height: 8.h,),
+          Text(
+            widget.uploadStatus,
+            style: TextStyle(
+              fontSize: 12.sp,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Satoshi',
+              color: AppColors.lightGrey,
+            ),
+          ),
+          SizedBox(height: 8.h),
           Text(
             'Kategori Surat',
             style: TextStyle(
