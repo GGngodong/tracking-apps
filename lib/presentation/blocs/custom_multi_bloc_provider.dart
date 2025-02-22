69d80cd59ed11e0bfdfbd6fdc29238b14fac7ecb
GGngodong <ikkoy123@gmail.com>
2025-02-23 03:28:45 +0700

    detail and edit notification and list notification

--- lib/presentation/blocs/custom_multi_bloc_provider.dart
+++ lib/presentation/blocs/custom_multi_bloc_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tracking_apps/configs/network/notification/notification_service.dart';
 import 'package:tracking_apps/configs/network/user_service.dart';
+import 'package:tracking_apps/presentation/blocs/notification/all/notification_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/detail/get_detail_permit_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/edit/edit_bloc.dart';
 import 'package:tracking_apps/presentation/blocs/permit/listPermit/approved/get_approved_permit_bloc.dart';
@@ -14,6 +16,8 @@ import 'package:tracking_apps/presentation/blocs/profile/profile_bloc.dart';
 
 import 'auth/login/login_bloc.dart';
 import 'auth/register/register_bloc.dart';
+import 'notification/detail/notification_detail_bloc.dart';
+import 'notification/edit/notification_edit_bloc.dart';
 
 class CustomMultiBlocProvider extends StatelessWidget {
   final Widget child;
@@ -52,7 +56,16 @@ class CustomMultiBlocProvider extends StatelessWidget {
         BlocProvider(
           create: (context) =>
               PermitLetterLatestBloc(userService: UserService()),
-        )
+        ),
+        BlocProvider(
+            create: (context) =>
+                NotificationBloc(notificationService: NotificationService())),
+        BlocProvider(
+            create: (context) => NotificationDetailBloc(
+                notificationService: NotificationService())),
+        BlocProvider(
+            create: (context) =>
+                NotificationEditBloc(notificationService: NotificationService()))
       ],
       child: child,
     );
