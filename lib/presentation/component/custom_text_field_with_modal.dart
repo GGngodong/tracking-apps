516b414ce601c0a9d5f150fb9d988fb63858288a
GGngodong <ikkoy123@gmail.com>
2025-02-23 03:17:15 +0700

    create custom bottom choice chip for add notes

---
+++ lib/presentation/component/custom_text_field_with_modal.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_apps/configs/theme/app_colors.dart';
+
+class CustomTextFieldWithModal extends StatefulWidget {
+  final String header;
+  final String hintText;
+  final void Function(String)? onFieldSubmitted;
+  final TextEditingController? textController;
+  final void Function()? onTap;
+
+  const CustomTextFieldWithModal({
+    super.key,
+    required this.header,
+    required this.hintText,
+    this.onFieldSubmitted,
+    this.textController, this.onTap,
+  });
+
+  @override
+  State<CustomTextFieldWithModal> createState() =>
+      _CustomTextFieldWithModalState();
+}
+
+class _CustomTextFieldWithModalState extends State<CustomTextFieldWithModal> {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: AppColors.primary,
+          selectionColor: AppColors.primary.withOpacity(0.4),
+          selectionHandleColor: AppColors.primary,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.header,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w500,
+              fontFamily: 'Satoshi',
+              color: AppColors.primary,
+            ),
+          ),
+          SizedBox(
+            height: 8.h,
+          ),
+          GestureDetector(
+            onTap: () => widget.onTap!(),
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 14.h),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: AppColors.lightGrey,
+                  width: 1.w,
+                ),
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              child: Text(
+                    widget.hintText,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.sp,
+                  fontFamily: 'Satoshi',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
