7259fc2a6c1c6624010b91c4f78ab36264f71e3b
GGngodong <ikkoy123@gmail.com>
2025-05-05 09:57:19 +0700

    fix regex

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -5,7 +5,7 @@ class Constant {
   static final DateTime nullDate = DateTime.parse('0001-01-01T00:00:00Z').toUtc();
   static final usernameRegex = RegExp(r'^[a-zA-Z0-9._]{3,20}$');
   static final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-  static final passwordRegex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+  static final passwordRegex = RegExp(r'^.{8,}$');
   static final descriptionRegex = RegExp(r'^[a-zA-Z0-9._]{3,20}$');
   static final noPermitRegex = RegExp(r'^[A-Z]{1,2}\/\d{1,3}\/[A-Z]{1,2}\/\d{4}\/[\w-]+(?:\.\w+)*$');
   static final companyNameRegex = RegExp(r'^([A-Z]{2,}\s*)+((?:[A-Z]{2,}\s*)+)?$');
